1d84a4eaf88ac2e09ae5ad26a456bbcc6b4bff1c
Kayla <bikeykay@yahoo.com>
2017-09-26 23:33:41 -0400

    update resumebuildr.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var awesomeThoughts= "I am Kayla and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+ console.log(awesomeThoughts);
+ console.log(funThoughts);
