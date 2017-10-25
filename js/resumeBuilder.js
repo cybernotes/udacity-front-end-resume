d48776ee030d1809f1f1f1dd25c7b84d2b0e630f
cybernotes <kaylarserpa@gmail.com>
2017-10-24 22:43:47 -0400

    beautified code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-//  var awesomeThoughts= "I am Kayla and I am AWESOME!";
-// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
-//  console.log(awesomeThoughts);
-//  console.log(funThoughts);
 var data = "%data%";
 var bio = {
   "name": "Kayla Serpa",
